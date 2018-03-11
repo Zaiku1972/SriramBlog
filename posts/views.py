df58f41e641c227fb1d554a582548aa905ecc075
Sriram Arvind L <lsriramarvind95@hotmail.com>
2018-03-12 00:07:04 +0530

    added slug and user check

--- posts/views.py
+++ posts/views.py
@@ -31,6 +31,8 @@ def posts_list(request):
     return render(request, "post_list.html", context)
 
 def posts_create(request):
+    if not request.user.is_staff or request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit= False)
@@ -44,16 +46,18 @@ def posts_create(request):
     }
     return render(request,"post_form.html",context)
 
-def posts_detail(request, id = None):
-    instance = get_object_or_404(Post, id = id )
+def posts_detail(request, slug = None):
+    instance = get_object_or_404(Post, slug = slug )
     context = {
         "title" : instance.title,
         "instance" : instance
     }
     return render(request,"post_detail.html",context)
 
-def posts_update(request, id = None):
-    instance = get_object_or_404(Post, id=id)
+def posts_update(request, slug = None):
+    if not request.user.is_staff or request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None,request.FILES or None,instance = instance)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -66,8 +70,8 @@ def posts_update(request, id = None):
 
     }
     return render(request, "post_form.html", context)
-def posts_delete(request, id = None):
-    instance = get_object_or_404(Post, id=id)
+def posts_delete(request, slug = None):
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request,"Post Deleted")
     return redirect("posts:list")
